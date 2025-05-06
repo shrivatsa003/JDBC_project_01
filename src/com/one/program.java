fdc67c8068fee84dee67fe9798f98a340102f857
shrivatsa003 <152469950+shrivatsa003@users.noreply.github.com>
2025-05-06 09:34:56 +0530

    Update program.java

--- src/com/one/program.java
+++ src/com/one/program.java
@@ -1,11 +1,11 @@
 package com.one;
-
+import java.util.Scanner;
 public class program {
 	public static void main(String[] args) {
-		System.out.println("hello word");
+		System.out.println("hello world");
 		Scanner s = new Scanner(System.in);
 		//Square of a number
-		System.out.println("Eter a number:");
+		System.out.println("Enter a number:");
 		int n = s.nextInt();
 		System.out.println("Square of the number is "+ (n*n));
 		
