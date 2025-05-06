5bed91d3b8825e9737a0f128f69f12223d12fc3e
shrivatsa003 <152469950+shrivatsa003@users.noreply.github.com>
2025-05-06 09:28:41 +0530

    Update program.java
    Square  of number is added

--- src/com/one/program.java
+++ src/com/one/program.java
@@ -3,5 +3,11 @@ package com.one;
 public class program {
 	public static void main(String[] args) {
 		System.out.println("hello word");
+		Scanner s = new Scanner(System.in);
+		//Square of a number
+		System.out.println("Eter a number:");
+		int n = s.nextInt();
+		System.out.println("Square of the number is "+ (n*n));
+		
 	}
 }
